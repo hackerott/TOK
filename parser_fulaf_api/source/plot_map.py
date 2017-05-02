097f8f4f95b216e588ae19ab68fb8dc244b2309d
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-05-02 18:09:54 -0300

    Add RMSE and BIAS (for real), change variables names to avoid misleading

--- parser_fulaf_api/source/plot_map.py
+++ parser_fulaf_api/source/plot_map.py
@@ -25,22 +25,39 @@ def DATA_get(rain_station, rain_wrf, lat, lon):
 	rc = [65, 15]
 	
 	return(rain_wrf, rain_station, lat, lon, lc, rc )	
+################################################################################
+## calculate the RMSE
+def _get_rmse(rain_station, rain_wrf):
+	r =  []
+	for i in range(0, len(rain_station)):
+		r.append((rain_station[i] - rain_wrf[i])**2)
+	return np.sqrt(np.mean(r))
 
 ################################################################################
-## create RMSE scatter size and color scheme
-def get_RMSE_plot(rain_wrf, rain_station):
+## calculate the BIAS
+def _get_bias(rain_station, rain_wrf):
+	b = []
+	n = 0
+	for i in range(0, len(rain_station)):
+		b.append(rain_station[i] - rain_wrf[i])
+		n += 1
+	return sum(b)/n
+################################################################################
+## create scatter size and color scheme
+def get_SCATTER_plot(rain_wrf, rain_station):
 #	size_r	= []
 #	size_b	= []
+	color_a	= []
 	color_r	= []
-	color_b	= []
 	mrs = max(rain_station)
+	n = len(rain_station)
 	for i in range(len(rain_wrf)):
-		rmse = np.sqrt((rain_wrf[i] - rain_station[i])**2)
- 		bias = (rain_wrf[i]- rain_station[i])/mrs 
-		color_r.append(rmse)
-		color_b.append(bias)
-	
-	return(color_b, color_r)
+		absolute_e = np.absolute((rain_wrf[i] - rain_station[i]))
+ 		relative_e = (rain_wrf[i]- rain_station[i])/mrs 
+		color_a.append(absolute_e)
+		color_r.append(relative_e)
+
+	return(color_r, color_a)
 
 #		size_r.append(10 + np.absolute(rmse/mrs)*10)
 #		size_b.append(10 + np.absolute(bias/mrs)*10)
@@ -51,11 +68,15 @@ def get_RMSE_plot(rain_wrf, rain_station):
 def DATA_plot_scatter(rain_wrf, rain_station, lat, lon, lc, rc, path4):
 	print "Setting RMSE and BIAS scale..."
 #	size_r, size_b, color_b, color_r  = get_RMSE_plot(rain_wrf, rain_station)
-	color_b, color_r  = get_RMSE_plot(rain_wrf, rain_station)
-	rmse = np.mean(color_r)
-	bias = np.mean(color_b)
+	color_r, color_a  = get_SCATTER_plot(rain_wrf, rain_station)
+
+	mae = np.mean(color_a)
+	mre = np.mean(color_r)
+	rmse = _get_rmse(rain_station, rain_wrf)
+	bias = _get_bias(rain_station, rain_wrf)
+
 	print "Setting basemaps..."
-	file_name_map = '%s/map_rmse_bias.png' % (path4)
+	file_name_map = '%s/map_mae_mre.png' % (path4)
 	file_name_scatter = '%s/scatter_obs_sim.png' % (path4)
 	file_name = '%s/model_simulation.png' % (path4)
 
@@ -70,10 +91,10 @@ def DATA_plot_scatter(rain_wrf, rain_station, lat, lon, lc, rc, path4):
 	plt.ylim(0, max(max(rain_wrf), max(rain_station)))
 	plt.scatter(rain_wrf, rain_station, c='black', marker='o', linewidth=0)
 	plt.plot(rain_station, rain_station, c='gray')
-	plt.figtext(0.73, 0.73, "Statistical indicators:\n\n Relative error: %.3f \n Absolute error: %.3f \n  " % (bias, rmse) ,bbox={'facecolor':'lightgray', 'alpha':0.5, 'pad':10}, fontsize=15, multialignment = 'left')
+	plt.figtext(0.73, 0.70, "Statistical indicators:\n\n MRE: %.3f\n MAE: %.3f\n Bias: %.3f\n RMSE: %.3f" % (mre, mae, bias, rmse) ,bbox={'facecolor':'lightgray', 'alpha':0.5, 'pad':10}, fontsize=15, multialignment = 'left')
 	plt.savefig(file_name_scatter, dpi=300, pad_inches=0)
 
-##RMSE Map
+##Absolute Error Map
 	fig = plt.figure("RMSE/BIAS SCATTER",figsize=(16, 9))
 	ax = fig.add_subplot(121)
 	ax.set_title("Absolute error (|sim - obs|) [mm]")
@@ -81,16 +102,16 @@ def DATA_plot_scatter(rain_wrf, rain_station, lat, lon, lc, rc, path4):
 	map1.drawcoastlines()
 	map1.drawcountries(linewidth=0.5, linestyle='solid', color='k', antialiased=1, ax=None, zorder=None)
 	map1.fillcontinents(color='lightgray', zorder=0)
-	print "Drawing RMSE..."	
-	map1.scatter(lon, lat, c=color_r, s=30, marker='o', latlon=True, linewidth=0, )
+	print "Drawing Absolute error..."	
+	map1.scatter(lon, lat, c=color_a, s=30, marker='o', latlon=True, linewidth=0, )
 	map1.colorbar(location='right', size='5%', pad='2%')
 	map1.drawmapscale(lc[1]+1.3, lc[0]+0.6, lc[1]+10, lc[0]+8, 250, barstyle='fancy', fontsize = 11) 
 	map1.drawparallels(np.arange(lc[0],rc[0]+2.5,2.5), labels=[1,0,0,1])
 	map1.drawmeridians(np.arange(lc[1],rc[1]+5,5.), labels=[1,0,0,1])
 
-##BIAS Map
-	color_b.append(1.0)
-	color_b.append(-1.0)
+##Realtive Error Map
+	color_r.append(1.0)
+	color_r.append(-1.0)
 	lat.append(0) 
 	lon.append(0)
 	lat.append(0)
@@ -101,8 +122,8 @@ def DATA_plot_scatter(rain_wrf, rain_station, lat, lon, lc, rc, path4):
 	map2.drawcoastlines()
 	map2.drawcountries(linewidth=0.5, linestyle='solid', color='k', antialiased=1, ax=None, zorder=None)
 	map2.fillcontinents(color='lightgray', zorder=0)
-	print "Drawing BIAS..."
-	map2.scatter(lon, lat, c=color_b, s=30, marker='o', latlon=True, linewidth=0, )
+	print "Drawing Relative error..."
+	map2.scatter(lon, lat, c=color_r, s=30, marker='o', latlon=True, linewidth=0, )
 	map2.colorbar(location='right', size='5%', pad='2%')
 	map2.drawmapscale(lc[1]+1.3, lc[0]+0.6, lc[1]+10, lc[0]+8, 250, barstyle='fancy', fontsize = 11) 
 	map2.drawparallels(np.arange(lc[0],rc[0]+2.5,2.5), labels=[1,0,0,1])
