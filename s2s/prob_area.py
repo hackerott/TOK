57b0450b22a93bd10dbafc9db484bab2ca0d429e
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-05-03 15:01:11 -0300

    Changing all the math to array base, using numpy to genarete roll grid with values. Testing threads, and general code simplify, making it more agnostic to diferent models

--- s2s/prob_area.py
+++ s2s/prob_area.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 
 import numpy as np
-import math
-import datetime
+import thread
+import netCDF4
 
 #######################################
 
@@ -20,7 +20,7 @@ def _get_NCVAR(var):
 
 #######################################
 ## return the probabilitys for each alert level
-def _get_Prob(var_raw, ix, iy, max_i):
+def _get_Prob(var_raw, ix, iy, max_i, top_lim, bot_lim):
 	prob_y = [0]*max_i
 	prob_g = [0]*max_i
 	prob_r = [0]*max_i
@@ -39,49 +39,72 @@ def _get_Prob(var_raw, ix, iy, max_i):
 
 #######################################	
 # return the final values of prob for each member
-def _get_AREA(ncfile, var, ix, iy):
+def _get_AREA(ncfile, var, ix, iy, top_lim, bot_lim):
 	time = ncfile.variables['time']
 	max_i = len(time)
 	top_lim =
 	bot_lim =
 	var_nc = _get_NCVAR(var)
-	
-
 	if len(var_nc) == 1:
-		var_raw = ncfile.variables[var_nc]
+		var_raw = ncfile.variables[var_nc] #single variable from nc
 
 	elif len(var_nc) == 2:
 		var_raw1 = ncfile.variables[var_nc[0]] 		
 		var_raw2 = ncfile.variables[var_nc[1]] 		
-		for i in range(0, max_i):
-			var_raw.append(sqrt(power(var_raw1[i, :, :], 2) + power(var_raw2[i, :, :], 2)))
+		var_raw = np.sqrt(np.add(np.power(var_raw1, 2), np.power(var_raw2, 2))) # wind intensity
 
 	elif len(var_nc) == 3:
 		var_raw1 = ncfile.variables[var_nc[0]] 		
 		var_raw2 = ncfile.variables[var_nc[1]] 		
 		var_raw3 = ncfile.variables[var_nc[2]]
-		for i in range(0, max_i):
-			var_raw.append(100*(var_raw1[i, :, :]/(379.90516/var_raw3[i, :, :] * exp(17.29*((var_raw2[i, :, :] - 273.15)/(var_raw2[i, :, :] - 35.86))))))
+		var_raw = np.multiply(np.multiply(100, np.divide(var_raw1, np.divide(379.90516, var_raw3))), np.exp(np.multiply(17.29, np.divide(np.add(var_raw2, -273.15), np.add(var_raw2, -35.86))))) # relative humidity
 
-#Center value gets 1/2 of prob, first 4 points get 1/4, and next 12 1/4
-	prob  = [0]*max_i 
-	value = [0]*max_i
-#######
-''' 
-Add aqui declaracao de todas as prob (A-0-1 ate A-2-12) via lançamento de tread, para agilizar a execucao, os valores sao independentes.
+#	prob	= [0]*max_i 
+	prob_r	= [0]*max_i 
+	prob_g	= [0]*max_i 
+	prob_y	= [0]*max_i 
+	value	= [0]*max_i
 
-http://softwareramblings.com/2008/06/running-functions-as-threads-in-python.html
+#prob goes from 1 to 25 
+	v_1, p_g1, p_r1, p_y1 = thread.start_new_thread(_get_Prob, (var_raw, ix, iy, max_i, top_lim, bot_lim))
 
-import thread
+	v_2, p_g2, p_r2, p_y2 = thread.start_new_thread(_get_Prob, (var_raw, ix, iy+1, max_i, top_lim, bot_lim))
+	v_3, p_g3, p_r3, p_y3 = thread.start_new_thread(_get_Prob, (var_raw, ix, iy-1, max_i, top_lim, bot_lim))
+	v_4, p_g4, p_r4, p_y4 = thread.start_new_thread(_get_Prob, (var_raw, ix+1, iy, max_i, top_lim, bot_lim))
+	v_5, p_g5, p_r5, p_y5 = thread.start_new_thread(_get_Prob, (var_raw, ix-1, iy, max_i, top_lim, bot_lim))
+	v_6, p_g6, p_r6, p_y6 = thread.start_new_thread(_get_Prob, (var_raw, ix+1, iy+1, max_i, top_lim, bot_lim))
+	v_7, p_g7, p_r7, p_y7 = thread.start_new_thread(_get_Prob, (var_raw, ix+1, iy-1, max_i, top_lim, bot_lim))
+	v_8, p_g8, p_r8, p_y8 = thread.start_new_thread(_get_Prob, (var_raw, ix-1, iy+1, max_i, top_lim, bot_lim))
+	v_9, p_g9, p_r9, p_y9 = thread.start_new_thread(_get_Prob, (var_raw, ix-1, iy-1, max_i, top_lim, bot_lim))
 
-thread.start_new_thread(_funcao, (argumentos)
+	v_10, p_g10, p_r10, p_y10 = thread.start_new_thread(_get_Prob, (var_raw, ix+2, iy+2, max_i, top_lim, bot_lim))
+	v_11, p_g11, p_r11, p_y11 = thread.start_new_thread(_get_Prob, (var_raw, ix+2, iy+1, max_i, top_lim, bot_lim))
+	v_12, p_g12, p_r12, p_y12 = thread.start_new_thread(_get_Prob, (var_raw, ix+2, iy, max_i, top_lim, bot_lim))
+	v_13, p_g13, p_r13, p_y13 = thread.start_new_thread(_get_Prob, (var_raw, ix+2, iy-1, max_i, top_lim, bot_lim))
+	v_14, p_g14, p_r14, p_y14 = thread.start_new_thread(_get_Prob, (var_raw, ix+2, iy-2, max_i, top_lim, bot_lim))
+	v_15, p_g15, p_r15, p_y15 = thread.start_new_thread(_get_Prob, (var_raw, ix+1, iy+2, max_i, top_lim, bot_lim))
+	v_16, p_g16, p_r16, p_y16 = thread.start_new_thread(_get_Prob, (var_raw, ix+1, iy-2, max_i, top_lim, bot_lim))
+	v_17, p_g17, p_r17, p_y17 = thread.start_new_thread(_get_Prob, (var_raw, ix, iy+2, max_i, top_lim, bot_lim))
+	v_18, p_g18, p_r18, p_y18 = thread.start_new_thread(_get_Prob, (var_raw, ix, iy-2, max_i, top_lim, bot_lim))
+	v_19, p_g19, p_r19, p_y19 = thread.start_new_thread(_get_Prob, (var_raw, ix-1, iy+2, max_i, top_lim, bot_lim))
+	v_20, p_g20, p_r20, p_y20 = thread.start_new_thread(_get_Prob, (var_raw, ix-1, iy-2, max_i, top_lim, bot_lim))
+	v_21, p_g21, p_r21, p_y21 = thread.start_new_thread(_get_Prob, (var_raw, ix-2, iy+2, max_i, top_lim, bot_lim))
+	v_22, p_g22, p_r22, p_y22 = thread.start_new_thread(_get_Prob, (var_raw, ix-2, iy+1, max_i, top_lim, bot_lim))
+	v_23, p_g23, p_r23, p_y23 = thread.start_new_thread(_get_Prob, (var_raw, ix-2, iy, max_i, top_lim, bot_lim))
+	v_24, p_g24, p_r24, p_y24 = thread.start_new_thread(_get_Prob, (var_raw, ix-2, iy-1, max_i, top_lim, bot_lim))
+	v_25, p_g25, p_r25, p_y25 = thread.start_new_thread(_get_Prob, (var_raw, ix-2, iy-2, max_i, top_lim, bot_lim))
 
-'''
-	for i in  range(0, max_i):
-		prob[i] = ## soma de todas as prob com os pesos
-		value[i] = ## soma de todos os valores com os pesos
+#Center value gets 1/2 of prob, first 4 points get 1/4, and next 12 1/4	
+
+	prob_g = np.add(np.divide(p_g1, 2), np.add(np.divide(np.add(np.add(np.add(p_g2, p_g3), np.add(p_g4, p_g5)), np.add(np.add(p_g6, p_g7), np.add(p_g8, p_g9))), 3), np.divide(np.add(np.add(np.add(np.add(p_g10, p_g11), np.add(p_g12, p_g13)), np.add(np.add(p_g14, p_g15), np.add(p_g16, p_g17))), np.add(np.add(np.add(p_g18, p_g19), np.add(p_g20, p_g21)), np.add(np.add(p_g22, p_g23), np.add(p_g24, p_g25)))), 6)))
+
+	prob_r = np.add(np.divide(p_r1, 2), np.add(np.divide(np.add(np.add(np.add(p_r2, p_r3), np.add(p_r4, p_r5)), np.add(np.add(p_r6, p_r7), np.add(p_r8, p_r9))), 3), np.divide(np.add(np.add(np.add(np.add(p_r10, p_r11), np.add(p_r12, p_r13)), np.add(np.add(p_r14, p_r15), np.add(p_r16, p_r17))), np.add(np.add(np.add(p_r18, p_r19), np.add(p_r20, p_r21)), np.add(np.add(p_r22, p_r23), np.add(p_r24, p_r25)))), 6)))
+
+	prob_y = np.add(np.divide(p_y1, 2), np.add(np.divide(np.add(np.add(np.add(p_y2, p_y3), np.add(p_y4, p_y5)), np.add(np.add(p_y6, p_y7), np.add(p_y8, p_y9))), 3), np.divide(np.add(np.add(np.add(np.add(p_y10, p_y11), np.add(p_y12, p_y13)), np.add(np.add(p_y14, p_y15), np.add(p_y16, p_y17))), np.add(np.add(np.add(p_y18, p_y19), np.add(p_y20, p_y21)), np.add(np.add(p_y22, p_y23), np.add(p_y24, p_y25)))), 6)))
+
+ 	value = np.add(np.divide(v_1, 2), np.add(np.divide(np.add(np.add(np.add(v_2, v_3), np.add(v_4, v_5)), np.add(np.add(v_6, v_7), np.add(v_8, v_9))), 3), np.divide(np.add(np.add(np.add(np.add(v_10, v_11), np.add(v_12, v_13)), np.add(np.add(v_14, v_15), np.add(v_16, v_17))), np.add(np.add(np.add(v_18, v_19), np.add(v_20, v_21)), np.add(np.add(v_22, v_23), np.add(v_24, v_25)))), 6)))
 
 	max_value = np.mean(value) + np.std(value)
 	min_value = np.mean(value) - np.std(value)
 
-	return(prob, value, max_value, min_value)
+	return(prob_g, prob_r, prob_y, value, max_value, min_value)
