0975698b5c63b5e90d2786b3f123c7e75ae0840f
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-05-31 19:47:38 -0300

    Changes to fix previus errors, fix accumulated for 24hrs, fix macthing date, fix output array  Changes to be committed: 	modified:   source/calc_area_error.py

--- parser_fulaf_api/source/calc_area_error.py
+++ parser_fulaf_api/source/calc_area_error.py
@@ -8,18 +8,7 @@ import sys
 import datetime
 
 ################################################################################
-#def _get_accumulated_rain(wrf, iz, ix, iy, d1, d2):
-#	a = 0
-#	while d1 <= d2:
-#		d1 = d1 + datetime.timedelta(hours= 1)
-#		a += 1 
-#	days = a//24
-#	if days == 0:
-#		inic = days
-#	else:
-#		inic = (days * 24) - 24
-#	rain1 = []
-#	for i in range(inic, inic+24):
+##
 def _get_accumulated_rain(wrf, iz, ix, iy):
 	inic = iz-24
 	if inic < 0:
@@ -35,7 +24,7 @@ def _get_accumulated_rain(wrf, iz, ix, iy):
 
 	return(rain)
 ################################################################################
-#def _get_area_error(wrf, station, jz, iz, ix, iy, search, d1, d2):
+##
 def _get_area_error(wrf, station, jz, iz, ix, iy, search):
 	ix_max = wrf.shape[1]
 	iy_max = wrf.shape[2]
@@ -50,24 +39,15 @@ def _get_area_error(wrf, station, jz, iz, ix, iy, search):
 		if (iy + search) <= iy_max and (iy - search) >= iy_min:
 			for i in range((ix - search), (ix + search)):
 				for j in range((iy - search), (iy + search)):
-#					rain = _get_accumulated_rain(wrf, iz, i, j, d1, d2)
 					rain = _get_accumulated_rain(wrf, iz, i, j)
 					error_a = (np.divide(np.absolute(rain - station[jz]), max_)) 
 					error_d = np.sqrt(((i-ix)**2 + (j-iy)**2)) / search
 					error.append(0.5*error_a + 0.5*error_d)
-#	else:
-#		if ix + search <= ix_max and ix - search >= ix_min:
-#			if iy + search <= iy_max and iy - search >= iy_min:
-#				for i in range((ix - search), (ix + search)):
-#					for j in range((iy - search), (iy + search)):
-#						error_a = (np.divide(np.absolute((wrf[iz, i, j] - wrf[iz-1, i, j])- station[jz]), max_)) 
-#						error_d = np.sqrt(((i-ix)**2 + (j-iy)**2)) / search
-#						error.append(0.5*error_a + 0.5*error_d)
 	
 	return(error)
 
-
 ################################################################################
+##
 def DATA_get(wrf_file, ix, iy, station, date0, date1, date_station):
 
 	date1 =  datetime.datetime.strptime(date1, '%Y%m%d%H')
@@ -76,8 +56,6 @@ def DATA_get(wrf_file, ix, iy, station, date0, date1, date_station):
 
 	WRF_rain_ncu	= WRFfile.variables['RAINNC']
 	time		= WRFfile.variables['Times']  
-#	WRF_lat		= WRFfile.variables['XLAT_M'] 
-#	WRF_lon		= WRFfile.variables['XLONG_M'] 
 	resolution	= getattr(WRFfile, 'DX')
 
 	max_i	= len(time)
@@ -89,27 +67,7 @@ def DATA_get(wrf_file, ix, iy, station, date0, date1, date_station):
 			d1 = datetime.datetime.strptime(str(date0), '%Y%m%d%H') + datetime.timedelta(hours= i)
 			d2 = date_station[j]
 			if d1 == date1 and d2 == date1:
-#				error_ = _get_area_error(wrf, station, j, i, ix, iy, search, date0, d2)
 				error_ = _get_area_error(wrf, station, j, i, ix, iy, search)
 				error.append(min(error_))
 
-
-	max_j	= max_i // 24
-	a = 24
-	b = 0
-	for i in range(0, max_j):
-		if  a >= max_i:
-			break
-		error_final.append(np.mean(error[b,a]))  
-		b = a
-		a += 24
-
-	return(error_final)
-
-
-		
-
-
-
-
-
+	return(error)
