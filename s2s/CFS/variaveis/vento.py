1d47d9bd12fe055263102eed8fef4fe296f3a522
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-06-12 20:20:47 -0300

    New function to return all limits for the probabilities calculations  Changes to be committed: 	modified:   CFS/variaveis/vento.py

--- s2s/CFS/variaveis/vento.py
+++ s2s/CFS/variaveis/vento.py
@@ -10,6 +10,22 @@ import datetime
 from math import pi
 from numpy import cos, sin, arccos, power, sqrt, exp, arctan2, argmin, argmax, arctan, mean
 
+def prob_limits():
+'''
+Possible method:
+	-Use 50 percentile of the variable as base, add STD to top limit, subtract it for botton limit.
+	-Use 70 perentile as top limit and 30 percentile as botton limit
+	-Stract mean from a climatological database
+	-Use the seasonal mean from the closest station
+
+'''
+
+	TOP = 
+	BOT = 
+	PRO = 
+	
+	return(TOP, BOT, PRO)
+
 def calendario(CFS_E1, CFS_E2, CFS_E3, CFS_E4, CFS_E5, CFS_E6, CFS_E7, CFS_E8, iz, ixCFS, iyCFS, date0, utc0):
 	ens1 = netCDF4.Dataset(CFS_E1, 'r')
 	ens2 = netCDF4.Dataset(CFS_E2, 'r')
