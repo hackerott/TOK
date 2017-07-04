60c136ee4970d56cb4a578b0580cbecb03e10bb5
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-07-03 23:37:44 -0300

    Add new method to calculate max an min values 	modified:   prob_time.py

--- s2s/prob_time.py
+++ s2s/prob_time.py
@@ -55,12 +55,20 @@ def _get_TIMEP(var_raw1, var_raw2, iz, ix, iy, top_lim, bot_lim):
 	max_i = len(var_raw1[:, ix, iy])
 
 	value, prob_g, prob_r, prob_y = _get_Prob(var_raw1, var_raw2, ix, iy, max_i, top_lim, bot_lim)
-	max_value = np.mean(value) + np.std(value)
-	min_value = np.mean(value) - np.std(value)
-
+	# max_value = np.mean(value) + np.std(value)
+	# min_value = np.mean(value) - np.std(value)
+        std_value = np.std(value)
+        max_value = []
+        min_value = []
+        for i in range(0, len(value)):
+                max_value.append((value[i]+std_value))
+                min_value.append((value[i]-std_value))
 	return(prob_g, prob_r, prob_y, value, max_value, min_value)
 
+
+
 def _get_FIG(var_raw1, var_raw2, iz, ix, iy)
+
 	max_i = len(var_raw1[:, ix, iy])
 
 	value, prob_g, prob_r, prob_y = _get_Prob(var_raw1, var_raw2, ix, iy, max_i, 1, 1)
