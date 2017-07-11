bc39998b214cce144f31c4a298344081aa9458a0
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-07-11 12:26:19 -0300

    =( Color bar still beating me 	modified:   source/plot_map.py

--- parser_fulaf_api/source/plot_map.py
+++ parser_fulaf_api/source/plot_map.py
@@ -155,10 +155,14 @@ def DATA_plot_scatter(rain_wrf, rain_station, lat, lon, lc, rc, path4, area_erro
 ##Model Map
 	lon.append(0.0)
 	lat.append(0.0)
-	rain_wrf.append(15.0)
+
 	for i in range(0, len(rain_station)):
 		if rain_station[i] > 15.0:
 			rain_station[i] = 15.0
+		if rain_wrf[i] > 15.0:
+			rain_wrf[i] = 15.0
+
+	rain_wrf.append(15.0)
 	rain_station.append(15.0)
 	norm_error.append(0.0)
 	area_error.append(0.0)
