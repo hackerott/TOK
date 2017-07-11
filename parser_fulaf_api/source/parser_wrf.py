327c571736bf65ab3348a1ac0251f6be51db3348
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-07-11 09:31:04 -0300

    Removing bilenear interpolation, a changing date validation. Checking why outputs are empty  Changes to be commited: 	modified:   source/parser_wrf.py

--- parser_fulaf_api/source/parser_wrf.py
+++ parser_fulaf_api/source/parser_wrf.py
@@ -22,17 +22,18 @@ def DATA_get(wrf_file, ix, iy, date0, date1):
 	for i in range(0, max_i):
 		if i == 0:
 			rain1 = WRF_rain_ncu[i, ix, iy]
-			rain2 = WRF_rain_ncu[i, ix+1, iy]
-			rain3 = WRF_rain_ncu[i, ix+1, iy+1]
-			rain4 = WRF_rain_ncu[i, ix, iy+1]
+#			rain2 = WRF_rain_ncu[i, ix+1, iy]
+#			rain3 = WRF_rain_ncu[i, ix+1, iy+1]
+#			rain4 = WRF_rain_ncu[i, ix, iy+1]
 
 		else:
 			rain1 = WRF_rain_ncu[i, ix, iy]		- WRF_rain_ncu[i - 1, ix, iy]
-			rain2 = WRF_rain_ncu[i, ix+1, iy]	- WRF_rain_ncu[i - 1, ix+1, iy]
-			rain3 = WRF_rain_ncu[i, ix+1, iy+1]	- WRF_rain_ncu[i - 1, ix+1, iy+1]
-			rain4 = WRF_rain_ncu[i, ix, iy+1]	- WRF_rain_ncu[i - 1, ix, iy+1]
-		rain_i = (rain1+rain2+rain3+rain4)/4
-		rain.append(rain_i)
+#			rain2 = WRF_rain_ncu[i, ix+1, iy]	- WRF_rain_ncu[i - 1, ix+1, iy]
+#			rain3 = WRF_rain_ncu[i, ix+1, iy+1]	- WRF_rain_ncu[i - 1, ix+1, iy+1]
+#			rain4 = WRF_rain_ncu[i, ix, iy+1]	- WRF_rain_ncu[i - 1, ix, iy+1]
+#		rain_i = (rain1+rain2+rain3+rain4)/4
+#		rain.append(rain_i)
+		rain.append(rain1)
 		d1 = datetime.datetime.strptime(str(date0), '%Y%m%d%H') + datetime.timedelta(hours= i)
 		date.append(d1)
 
@@ -48,12 +49,12 @@ def DATA_get(wrf_file, ix, iy, date0, date1):
 	for j in range(0, max_j):
 #		if a > max_i:
 #			break
-		if date[b] == date1:
+		if date[a] == date1:
 			out_self = sum(rain[b:a])
 			if out_self < 0:
 				out_self = 0
 			out.append(out_self)
-			date_f.append(date[b])
+			date_f.append(date[a])
 			lat.append(WRF_lat[ix, iy])
 			lon.append(WRF_lon[ix, iy])
 		b = a
