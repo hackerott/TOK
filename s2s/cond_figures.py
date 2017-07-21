aeb564eaa6fcee674ebea3b5fc60ab7445207d50
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-07-21 17:17:37 -0300

    Adding dic with paths for all figures 	modified:   cond_figures.py

--- s2s/cond_figures.py
+++ s2s/cond_figures.py
@@ -3,7 +3,25 @@
 
 def _get_figure_url(var):
 	return{
-	
+			"1"	:	"http://50.112.50.113/figuras/images_sky/clo.png",
+			"2"	:	"http://50.112.50.113/figuras/images_sky/clo_r.png",
+			"3"	:	"http://50.112.50.113/figuras/images_sky/clo_s.png",
+			"4"	:	"http://50.112.50.113/figuras/images_sky/clo_t.png",
+			"5"	:	"http://50.112.50.113/figuras/images_sky/moo.png",
+			"6"	:	"http://50.112.50.113/figuras/images_sky/moo_f_c.png",
+			"7"	:	"http://50.112.50.113/figuras/images_sky/moo_h.png",
+			"8"	:	"http://50.112.50.113/figuras/images_sky/moo_m_c.png",
+			"9"	:	"http://50.112.50.113/figuras/images_sky/moo_r.png",
+			"10":	"http://50.112.50.113/figuras/images_sky/moo_s.png",
+			"11":	"http://50.112.50.113/figuras/images_sky/moo_t.png",
+			"12":	"http://50.112.50.113/figuras/images_sky/sun.png",
+			"13":	"http://50.112.50.113/figuras/images_sky/sun_f_c.png",
+			"14":	"http://50.112.50.113/figuras/images_sky/sun_h_c.png",
+			"15":	"http://50.112.50.113/figuras/images_sky/sun_m_c.png",
+			"16":	"http://50.112.50.113/figuras/images_sky/sun_r.png",
+			"17":	"http://50.112.50.113/figuras/images_sky/sun_s.png",
+			"18":	"http://50.112.50.113/figuras/images_sky/sun_t.png",
+			
 	}.get(var, 'Null')
 
 def DATA_wrf(cloud, rain, date):
