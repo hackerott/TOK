234a7911eced8d3d027d7716c3d11083df646ffe
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-07-26 13:15:43 -0300

    New script to return sun_rise, sun_set and utc offset from lat an lon. This makes all timestemps based on locations. No more need, to user imput, unless it is wanted.
     Changes to be committed:
    	new file:   astro_tz.py

---
+++ s2s/astro_tz.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#import numpy as np
+import timezonefinder as tz
+import datetime
+import pytz
+import ephem
+
+def _get_timezone(lat, lon):
+	
+	tf = tz.TimezoneFinder()
+	loc = tf.timezone_at(lng=lat, lat=lon)
+	local = pytz.timezone(loc)
+	off_set = datetime.datetime.now(local)
+	utc = off_set.utcoffset().total_seconds()//3600
+
+	return(int(utc))
+
+def _get_sun(lat, lon, utc):
+
+	observer = ephem.Observer()
+	observer.lat 	= str(lat)
+	observer.long 	= str(lon)
+	observer.date	= datetime.datetime.now().strftime('%Y/%m/%d')
+
+	sun = ephem.Sun(observer)
+	sun_rise	= observer.next_rising(sun)
+	sun_set		= observer.next_setting(sun)
+	sun_rise = ephem.Date(sun_rise(sun, start=observer.date) + utc*ephem.hour).datetime().strftime('%H:%M')
+	sun_set  = ephem.Date( sun_set(sun, start=observer.date) + utc*ephem.hour).datetime().strftime('%H:%M')
+
+	return(sun_rise, sun_set)
+
