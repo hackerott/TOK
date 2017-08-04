Fix for handling decimal cases on units Fix for vertical var on wrf_var and main_wrf Added proper error messages on json_out  Changes to be committed: 	modified:   json_output.py 	modified:   main_wrf.py 	modified:   units.py 	modified:   wrf_var.py

s2s/main_wrf.py
@@ -124,8 +124,8 @@ elif var_id == 5:
 		success, dic = json_output._get_ERROR(var_id, model) 
 
 elif var_id == 6:
-	var_rawa1, var_rawb1 = wrf_var._get_figure(var, ens1)
-	var_rawa2, var_rawb2 = wrf_var._get_figure(var, ens2)
+	var_rawa1, var_rawb1 = wrf_var._get_figure(var, ens1, ix_wrf, iy_wrf)
+	var_rawa2, var_rawb2 = wrf_var._get_figure(var, ens2, ix_wrf, iy_wrf)
 	time  	 = cfs_var._get_time('time', ens1)
 	var_rawa1 = np.max(var_rawa1, axis=0) 
 	var_rawa2 = np.max(var_rawa2, axis=0) 

s2s/units.py
@@ -6,23 +6,27 @@ import numpy as np
 #######################################'
 ## return the variables end value in imperial system 
 def _get_imperial(value, var_id):
+	cur = "imperial"
 	if var_id == 1:
 		value = np.multiply(value, 0.036)
-		cur = "inches"
 	elif var_id == 2:
-		value = np.multiply(value, 2,23694)
-		cur = "mph"
+		value_1 = np.multiply(value[0,:], 2,23694)
+		out = []
+		for val, i in value_1:
+			if val < 1 and val > 0:
+				val = int((val *10))/10.0
+				val = [val, int(value[1, i])] 
+			else:
+				val = [int(val), int(value[1, i])] #probably add to much error
+			out.append(val)
+		return(out, cur)
 	elif var_id == 3:	
 		value = np.add(np.multiply(value, 1.8), 32)
-		cur = "F"
-	elif var_id == 4:	
-#		value = np.multiply(value, )
-		cur = "watts"
-	elif var_id == 5:	
-#		value = np.multiply(value,
-		cur = "%"
+	elif var_id <= 5:	
+		cur = "metric"
+		value = value
 	else:
-		cur = ""
+		cur = "metric"
 		return(value, cur)
 	out = []
 	for val in value:
@@ -31,30 +35,29 @@ def _get_imperial(value, var_id):
 		else:
 			val = int(val) #probably add to much error
 		out.append(val)
-
 	return(out, cur)
 #######################################'
 ## return the variables end value metric
 def _get_metric(value, var_id):
-	if var_id == 1:
-		cur = "mm"
-	elif var_id == 2:
-		cur = "m/s"
-	elif var_id == 3:	
-		cur = "C"
-	elif var_id == 4:	
-		cur = "watts"
-	elif var_id == 5:	
-		cur = "%"
+	cur = "metric"
+	if var_id <= 5:
+		value = value
+	elif var_id == 2:	
+		for val, i in value[0,:]:
+			if val < 1 and val > 0:
+				val = int((val *10))/10.0
+				val = [val, int(value[1, i])] 
+			else:
+				val = [int(val), int(value[1, i])] #probably add to much error
+			out.append(val)
+		return(out, cur)
 	else:
-		cur = ""
 		return(value, cur)
 	out = []
 	for val in value:
 		if val < 1 and val > 0:
 			val = int((val *10))/10.0
 		else:
-			val = int(val)#probably add to much error
+			val = int(val) #probably add to much error
 		out.append(val)
-		
 	return(out, cur)

s2s/wrf_var.py
@@ -42,6 +42,7 @@ def _get_LIM(var):
 		'temp'		: [0.4, 27, 20],
 		'radiacao'	: [0.4, 1400, 800],
 		'umidade'	: [0.4, 0.3, 0.7]
+		'figura'	: [0.4, 10, 0.5],
 		}
 	out =  DIC.get(var, ['Null', 'Null', 'Null'])
 	return(out[0], out[1], out[2])
@@ -159,20 +160,18 @@ def _get_figure(var, ncfile, ix, iy):
 		var_raw2 = ncfile.variables[var_nc[1]]	
 		var_raw3 = ncfile.variables[var_nc[2]]	
 		var_rawa = np.add(var_raw2, var_raw3)
-		var_rawb = var_raw2
+		var_rawb = var_raw3
 		for i in range(0, len(var_raw2)):
 			if i == 0:
 				var_rawa[i,:,:] = np.add(var_raw2[i,:,:], var_raw3[i,:,:])
 			else:
 				var_rawa[i,:,:] = np.subtract(np.add(var_raw2[i,:,:], var_raw3[i,:,:]), np.add(var_raw2[i-1,:,:], var_raw3[i-1,:,:]))
-			var_rawb[i,:,:] = np.amax(var_raw1[i,:,:,:], axis=1)
 		for i in range(0, var_raw1.shape(0)):
 			# for i in range(0, var_raw1,shape(2)):
 			# 	for i in range(0, var_raw1.shape(3)):
 			for x in range(ix-4, ix+4):
 				for y in range(iy-4, iy+4):
 					var_rawb[i,x,y] = max(var_raw1[i,:,x,y])
-
 	except:
 		var_rawa = np.nan
 		var_rawb = np.nan