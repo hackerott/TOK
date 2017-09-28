aa1ab9274f79f401d3df3be79cbaab86f3ee0783
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-09-28 14:26:32 -0300

    Changes to be committed: 	modified:   s2s/main_gfs.py

--- s2s/main_gfs.py
+++ s2s/main_gfs.py
@@ -89,6 +89,7 @@ if var_id == 1 :
 		value, date = interpol._get_gfs_days(value, date)
 	elif model == "gcard":
 		date, prob, alert, value, maxi, mini = gcard.DATA_gfs_gcard(var_raw1, var_raw2, time, ix_gfs, iy_gfs, date0, utc0, TOP, BOT, PRO, var_id)
+		value, date = interpol._get_gfs_days(value, date)
 	else:
 		success, dic = json_output._get_ERROR(var_id, model) 
 
@@ -114,12 +115,11 @@ elif var_id == 2:
 		value = np.array(value)
 	elif model == "gcard":
 		date, prob, alert, value, maxi, mini = gcard.DATA_gfs_gcard(var_rawa1, var_rawa2, time, ix_gfs, iy_gfs, date0, utc0, TOP, BOT, PRO, var_id)
-		value1, date = interpol._get_gfs_days(value, date)
+		value, date = interpol._get_gfs_days(value, date)
 		# value2, date = interpol._get_gfs_days(var_rawb1[:, ix_gfs, iy_gfs], date)
 		# for i in range(0, len(value)):
 		# 	value[i] = [value1[i], int(var2[i])]
 		# value = np.array(value)
-		value = np.array(value1)
 	else:
 		success, dic = json_output._get_ERROR(var_id, model) 
 
