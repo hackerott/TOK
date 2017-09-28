Fixes for wind  Changes to be committed: 	modified:   s2s/gcard.py 	modified:   s2s/main_gfs.py 	modified:   s2s/units.py

s2s/gcard.py
@@ -77,7 +77,10 @@ def DATA_gfs_gcard(ens1, ens2, time, ixGFS, iyGFS, date0, utc0, TOP, BOT, PRO, v
 			value_t	= (np.subtract(value_t1[b], value_t1[a]))
 			value_p	= ((2*value_t  +  value_a)/3)
 		elif var_id == 2:
-			value_p = max_v
+			if np.isnan(max_v):
+				value_p = (value_t1 + value_a1)/2
+			else:		
+				value_p = max_v
 		elif var_id == 3:
 			value_p = [max_v, min_v]
 		elif var_id == 4:

s2s/main_gfs.py
@@ -107,10 +107,12 @@ elif var_id == 2:
 		value = np.array(value)
 	elif model == "table":
 		date, prob, alert, value, maxi, mini = table.DATA_gfs_table(var_rawa1, var_rawa2, time, ix_gfs, iy_gfs, date0, utc0, TOP, BOT, PRO)	
-		value1, date = interpol._get_gfs_days(value, date)
-		value2, date = interpol._get_gfs_days(var_rawb1[:, ix_gfs, iy_gfs], date)
-		for i in range(0, min(len(value1), len(value2))):
-			value[i] = [value1[i], int(value2[i])]
+#		value1, date = interpol._get_gfs_days(value, date)
+#		value2, date = interpol._get_gfs_days(var_rawb1[:, ix_gfs, iy_gfs], date)
+#		for i in range(0, min(len(value1), len(value2))):
+#			value[i] = [value1[i], int(value2[i])]
+		for i in range(0, len(value1)):
+			value[i] = [value[i], var_rawb1[i, ix_gfs, iy_gfs]]
 		value = np.array(value)
 	elif model == "gcard":
 		date, prob, alert, value, maxi, mini = gcard.DATA_gfs_gcard(var_rawa1, var_rawa2, time, ix_gfs, iy_gfs, date0, utc0, TOP, BOT, PRO, var_id)