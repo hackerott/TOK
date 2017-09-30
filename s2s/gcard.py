Fixing rain in gcard to accumulated not average  Changes to be committed: 	modified:   s2s/gcard.py

s2s/gcard.py
@@ -75,8 +75,8 @@ def DATA_gfs_gcard(ens1, ens2, time, ixGFS, iyGFS, date0, utc0, TOP, BOT, PRO, v
 		if var_id == 1:
 #			value_a	= (np.subtract(value_a1[b], value_a1[a]))
 #			value_t	= (np.subtract(value_t1[b], value_t1[a]))
-			value_a = np.add(value_a1[a:b])
-			value_t = np.add(value_t1[a:b])
+			value_a = np.sum(value_a1[a:b])
+			value_t = np.sum(value_t1[a:b])
 			value_p	= ((2*value_t  +  value_a)/3)
 		elif var_id == 2:
 			if np.isnan(max_v):
@@ -120,8 +120,10 @@ def DATA_wrf_gcard(ens1, ens2, time, ixWRF, iyWRF, date0, utc0, TOP, BOT, PRO, v
 		max_v	= max(np.nanmax(max_t1[a:b]), np.nanmax(max_a1[a:b]))
 		min_v	= min(np.nanmin(min_t1[a:b]), np.nanmin(min_a1[a:b]))
 		if var_id == 1:
-			value_a		= (np.subtract(value_a1[b], value_a1[a]))
-			value_t 	= (np.subtract(value_t1[b], value_t1[a]))
+#			value_a		= (np.subtract(value_a1[b], value_a1[a]))
+#			value_t 	= (np.subtract(value_t1[b], value_t1[a]))
+			value_a = np.sum(value_a1[a:b])
+			value_t = np.sum(value_t1[a:b])
 			value_p 	= ((2*value_t  +  value_a)/3)
 		elif var_id == 2:
 			value_p = max_v