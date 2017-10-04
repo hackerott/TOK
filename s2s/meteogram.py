16f11211e61e08e2f2def1f3c96960e3f44d1b82
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-10-04 12:04:26 -0300

    Changes to be committed: 	modified:   s2s/meteogram.py

--- s2s/meteogram.py
+++ s2s/meteogram.py
@@ -88,7 +88,10 @@ def DATA_gfs_meteo(temp1, temp2, wind1, wind2, humi1, humi2, cloud1, cloud2, rai
 	for i in range(0, max_i):
 		d1 = date0 + datetime.timedelta(hours = 0) + datetime.timedelta(hours = i) + datetime.timedelta(hours = utc0)
 		date.append(d1)
-		value.append([int(temp_p[i]*10)/10.0, int(wind_p[i]*10)/10.0, int(humi_p[i]*10)/10.0, int(cloud_p[i]*100), int(rain_p[i]*10)/10.0, int(pres_p[i]*10)/10.0, int(dew_p[i]*10)/10.0, cape, gust])
+		try:	
+			value.append([int(temp_p[i]*10)/10.0, int(wind_p[i]*10)/10.0, int(humi_p[i]*10)/10.0, int(cloud_p[i]*100), int(rain_p[i]*10)/10.0, int(pres_p[i]*10)/10.0, int(dew_p[i]*10)/10.0, cape, gust])
+		except:
+			value.append(['null','null','null','null','null','null','null','null','null'])	
 	del temp_p, wind_p, humi_p, cloud_p, rain_p, pres_p, dew_p, cape, gust
 	return(date, 'null', 'null', value, 'null', 'null')
 
@@ -158,11 +161,14 @@ def DATA_wrf_meteo(temp1, temp2, wind1, wind2, humi1, humi2, cloud1, cloud2, rai
 	for i in range(0, max_i):
 		d1 = date0 + datetime.timedelta(hours = 0) + datetime.timedelta(hours = i) + datetime.timedelta(hours = utc0)
 		date.append(d1)
-		try:
+		# try:
+		# 	value.append([int(temp_p[i]*10)/10.0, int(wind_p[i]*10)/10.0, int(humi_p[i]*10)/10.0, int(cloud_p[i]*100), int(rain_p[i]*10)/10.0, int(pres_p[i]*10)/10.0, int(dew_p[i]*10)/10.0, cape, gust])
+		# except:
+		# 	value.append([temp_p[i], wind_p[i], humi_p[i], cloud_p[i], rain_p[i], pres_p[i], dew_p[i], cape, gust])	
+		try:	
 			value.append([int(temp_p[i]*10)/10.0, int(wind_p[i]*10)/10.0, int(humi_p[i]*10)/10.0, int(cloud_p[i]*100), int(rain_p[i]*10)/10.0, int(pres_p[i]*10)/10.0, int(dew_p[i]*10)/10.0, cape, gust])
 		except:
-			value.append([temp_p[i], wind_p[i], humi_p[i], cloud_p[i], rain_p[i], pres_p[i], dew_p[i], cape, gust])	
-
+			value.append(['null','null','null','null','null','null','null','null','null'])	
 	del temp_p, wind_p, humi_p, cloud_p, rain_p, pres_p, dew_p, cape, gust
 	return(date, 'null', 'null', value, 'null', 'null')
 
