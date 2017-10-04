90575715de5795e22dd3e509a4c598afe4cd9acd
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-10-04 17:40:12 -0300

    Changes to be committed: 	modified:   s2s/units.py

--- s2s/units.py
+++ s2s/units.py
@@ -26,10 +26,10 @@ def _get_imperial(value, var_id):
 			value = np.multiply(value, 2.23694)
 	elif var_id == 3:	
 		if len(value[1]) > 0: ## ugly fix 
-			value1 = np.add(np.multiply(value[:,0], 1.8), 32)
-			value2 = np.add(np.multiply(value[:,1], 1.8), 32)
-			for i, val in enumerate(value1):
-				value[i] = [val, value2[i]]
+			for i, val in enumerate(value):
+				val1 = np.add(np.multiply(val[0], 1.8), 32)
+				val2 = np.add(np.multiply(val[1], 1.8), 32)
+				value[i] = [val1, val2]
 		else:
 			value = np.add(np.multiply(value, 1.8), 32)
 	elif var_id <= 5:	
