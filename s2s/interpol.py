e8b8c72f04b6cb8e93555d3c9ec3f1e1e7b6cc1f
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2017-10-06 12:41:30 -0300

    Same as before  Changes to be committed: 	modified:   s2s/interpol.py

--- s2s/interpol.py
+++ s2s/interpol.py
@@ -49,10 +49,9 @@ def _get_cfs_days(val, dat):
 	for i in range(0, len(val)):
 		value.append(int(val[i]*10)/10.0)
 		date.append(dat[i])
-			for j in range(1, 8):
-				value.append(np.nan)
-				date.append(dat[i] + datetime.timedelta(hours = i))
-
+		for j in range(1, 8):
+			value.append(np.nan)
+			date.append(dat[i] + datetime.timedelta(hours = i))
 	index = np.arange(len(value))
 	not_nan = np.logical(np.isnan(value))
 	out = InterpolatedUnivariateSpline(index[not_nan], value[not_nan], k=5)
